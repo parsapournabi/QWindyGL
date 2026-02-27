cd0dd0e5c1554be634c665c201747a9f821aefd6
Wearily <parsapournabi1382@gmail.com>
2026-02-27 09:14:41 +0330

    modify files structures

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -14,11 +14,14 @@ int main(int argc, char *argv[])
         &engine,
         &QQmlApplicationEngine::objectCreated,
         &app,
-        [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
+        [url](QObject * obj, const QUrl & objUrl)
+    {
+        if (!obj && url == objUrl)
+        {
+            QCoreApplication::exit(-1);
+        }
+    },
+    Qt::QueuedConnection);
     engine.load(url);
 
     return app.exec();
