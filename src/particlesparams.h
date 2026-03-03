fb4a24bdc3ce694de70a4e17d4aa7169fee94d2b
Wearily <parsapournabi1382@gmail.com>
2026-03-03 19:42:20 +0330

    adding new properties for ParticlesParams object
    added: windMin, windMax, windDataIndex property.

--- src/particlesparams.h
+++ src/particlesparams.h
@@ -2,6 +2,7 @@
 #define PARTICLESPARAMS_H
 
 #include <QObject>
+#include <QVector2D>
 
 // Suprer Fast Property definition
 #define PARTICLE_PROP(type, name, cname, value) \
@@ -10,17 +11,16 @@
     public: \
         type name() const { return m_##name; } \
         void set##cname(type v) { \
-            if (v == m_##name) \
-                return; \
+            if (m_##name == v) return; \
             m_##name = v; \
-            m_##name##HasChanged = true; \
+            name##HasChanged = true; \
             emit name##Changed(); \
         } \
         Q_SIGNAL void name##Changed(); \
+        /* non-QObject classes Will recognize changes by this variable */ \
+        bool name##HasChanged = false; \
     private: \
         type m_##name = value; \
-        bool m_##name##HasChanged = false; // non-QObject classes Will recognize changes by this variable
-
 
 class ParticlesParams : public QObject
 {
@@ -30,6 +30,9 @@ class ParticlesParams : public QObject
         PARTICLE_PROP(float, speedFactor, SpeedFactor, 0.0)
         PARTICLE_PROP(float, dropRate, DropRate, 0.0)
         PARTICLE_PROP(float, dropRateBump, DropRateBump, 0.0)
+        PARTICLE_PROP(QVector2D, windMin, WindMin, QVector2D(-10.0, -10.0)) // UMin, VMin
+        PARTICLE_PROP(QVector2D, windMax, WindMax, QVector2D(10.0, 10.0)) // UMax, VMax
+        PARTICLE_PROP(int, windDataIndex, WindDataIndex, -1)
     public:
         explicit ParticlesParams(QObject* parent = nullptr)
             : QObject{parent}
